0b9b6af2410f5daeaeea0a5f761b6131a7c64060
slipstreamJumper <the.slipstream.jumper@gmail.com>
2020-10-25 21:57:32 -0400

    fixed bug in mapbox call

--- src/app/components/my-map/my-map.component - Copy.ts
+++ src/app/components/my-map/my-map.component - Copy.ts
@@ -1,46 +1,38 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import * as L from 'leaflet';
+import  * as L from 'leaflet';
 import 'mapbox-gl-leaflet';
-
 @Component({
   selector: 'app-my-map',
   templateUrl: './my-map.component.html',
   styleUrls: ['./my-map.component.scss']
 })
 export class MyMapComponent implements OnInit, AfterViewInit {
-
   private map: L.Map;
-
   @ViewChild('map')
   private mapContainer: ElementRef<HTMLElement>;
-
   constructor() { }
-
   ngOnInit() {
   }
-
   ngAfterViewInit() {
-    const myAPIKey = '23f38da328ed40c08d05ae637d27e626';
-    const mapStyle = "https://maps.geoapify.com/v1/styles/osm-carto/style.json";
-
+    const myAPIKey = "449cbe9951e44776b4d615a923149aca";
+    const mapStyle = "https://maps.geoapify.com/v1/styles/dark-matter-dark-purple/style.json";
     const initialState = {
-      lat: 38.88,
-      long: -77.05,
+      lng: 11,
+      lat: 49,
       zoom: 4
     };
-
     const map = new L.Map(this.mapContainer.nativeElement).setView(
-      [initialState.lat, initialState.long],
+      [initialState.lat, initialState.lng],
       initialState.zoom
     );
-
+    // the attribution is required for the Geoapify Free tariff plan
     map.attributionControl
       .setPrefix("")
-      .addAttribution("Geoapify"
+      .addAttribution(
+        'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>'
       );
-
     L.mapboxGL({
-      style: '${mapStyle}?apiKey=${myAPIKey}',
+      style: `${mapStyle}?apiKey=${myAPIKey}`,
       accessToken: "no-token"
     }).addTo(map);
   }
--- src/app/components/my-map/my-map.component.ts
+++ src/app/components/my-map/my-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import  * as L from 'leaflet';
+import * as L from 'leaflet';
 import 'mapbox-gl-leaflet';
 @Component({
   selector: 'app-my-map',
@@ -14,25 +14,25 @@ export class MyMapComponent implements OnInit, AfterViewInit {
   ngOnInit() {
   }
   ngAfterViewInit() {
-    const myAPIKey = "449cbe9951e44776b4d615a923149aca";
-    const mapStyle = "https://maps.geoapify.com/v1/styles/osm-carto/style.json";
+    const myAPIKey = '449cbe9951e44776b4d615a923149aca';
+    const mapStyle = "https://maps.geoapify.com/v1/styles/dark-matter-dark-purple/style.json";
     const initialState = {
-      lng: 11,
-      lat: 49,
+      lat: 38.88,
+      long: -77.05,
       zoom: 4
     };
     const map = new L.Map(this.mapContainer.nativeElement).setView(
-      [initialState.lat, initialState.lng],
+      [initialState.lat, initialState.long],
       initialState.zoom
     );
-    // the attribution is required for the Geoapify Free tariff plan
+
     map.attributionControl
       .setPrefix("")
-      .addAttribution(
-        'Powered by <a href="https://www.geoapify.com/" target="_blank">Geoapify</a> | © OpenStreetMap <a href="https://www.openstreetmap.org/copyright" target="_blank">contributors</a>'
+      .addAttribution("Geoapify"
       );
+
     L.mapboxGL({
-      style: `${mapStyle}?apiKey=${myAPIKey}`,
+      style: '${mapStyle}?apiKey=${myAPIKey}',
       accessToken: "no-token"
     }).addTo(map);
   }
