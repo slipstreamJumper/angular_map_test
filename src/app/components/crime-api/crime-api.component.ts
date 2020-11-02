f14edf485ba92556f764afb586090eb83ea47c48
slipstreamJumper <the.slipstream.jumper@gmail.com>
2020-11-01 20:45:34 -0500

    added crime-api component

--- src/app/components/crime-api/crime-api.component.ts
+++ src/app/components/crime-api/crime-api.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-crime-api',
@@ -10,8 +12,8 @@ export class CrimeApiComponent {
   URL: string = 'https://maps2.dcgis.dc.gov/dcgis/rest/services/DCGIS_DATA/Public_Safety_WebMercator/MapServer/32/query?where=1%3D1&f=json';
 
   resolveItems = function (){
-    console.log(this.http.get(this.URL));
-    return this.http.get(this.URL);
+    //console.log(this.http.get(this.URL));
+    return this.http.get(URL);
   };
 
   constructor() {
